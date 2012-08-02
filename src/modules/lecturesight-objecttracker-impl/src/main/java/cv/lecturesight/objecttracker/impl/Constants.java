89ef321cbf900097470ae28083cb49afe9ee46bb
bwulff <bwulff@e6edf6fb-f266-4316-afb4-e53d95876a76>
2012-08-02 20:36:16 +0000

    LECTURESIGHT-23: yea svn..
    git-svn-id: http://localhost/repos/opencast/LECTURESIGHT/trunk@12635 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ src/modules/lecturesight-objecttracker-impl/src/main/java/cv/lecturesight/objecttracker/impl/Constants.java
@@ -0,0 +1,17 @@
+package cv.lecturesight.objecttracker.impl;
+
+public class Constants {
+
+  static final String SIGNAME_DONE = "objecttracker.DONE";
+  static final String PROPKEY_WIDTHMIN = "width.min";
+  static final String PROPKEY_WIDTHMAX = "width.max";
+  static final String PROPKEY_HEIGHTMIN = "height.min";
+  static final String PROPKEY_HEIGHTMAX = "height.max";
+  static final String PROPKEY_MATCHTHRESH = "regionmatch.threshold";
+  static final String PROPKEY_TTL = "ttl";
+  static final String OBJ_PROPKEY_REGION = "obj.region";
+  static final String OBJ_PROPKEY_BBOX = "obj.bbox";
+  static final String OBJ_PROPKEY_CENTROID = "obj.centroid";
+  static final String OBJ_PROPKEY_WEIGHT = "obj.weight";
+  
+}
