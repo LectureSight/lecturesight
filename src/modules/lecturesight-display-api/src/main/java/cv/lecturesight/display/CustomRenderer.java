12192e1f66371f0ec0e59f7be430798f43c533ea
bwulff <bwulff@e6edf6fb-f266-4316-afb4-e53d95876a76>
2012-08-27 09:26:46 +0000

    LECTURESIGHT-38: added display bundles
    git-svn-id: http://localhost/repos/opencast/LECTURESIGHT/trunk@12823 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ src/modules/lecturesight-display-api/src/main/java/cv/lecturesight/display/CustomRenderer.java
@@ -0,0 +1,8 @@
+package cv.lecturesight.display;
+
+import java.awt.Graphics;
+
+public interface CustomRenderer {
+
+  void render(Graphics g);
+}
---
+++ src/modules/lecturesight-display-impl/src/main/java/cv/lecturesight/display/impl/DisplayWindowImpl.java
@@ -0,0 +1,156 @@
+package cv.lecturesight.display.impl;
+
+import com.nativelibs4java.opencl.CLImage2D;
+import com.nativelibs4java.opencl.CLQueue;
+import cv.lecturesight.display.CustomRenderer;
+import cv.lecturesight.display.Display;
+import cv.lecturesight.opencl.OpenCLService;
+import cv.lecturesight.opencl.api.ComputationRun;
+import cv.lecturesight.opencl.api.OCLSignal;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.awt.image.BufferedImage;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class DisplayWindowImpl implements Display, WindowListener {
+
+  OpenCLService ocl;
+  OCLSignal trigger, SIG_done;
+  ComputationRun workingRun;
+  private CLImage2D image;
+  private CustomRenderer renderer = null;
+  private JFrame frame = new JFrame();
+  private JLabel label = new JLabel();
+  private boolean active = false;
+
+  public DisplayWindowImpl(String title, CLImage2D image) {
+    frame.setTitle(title);
+    this.image = image;
+    initComponents();
+    initRun();
+  }
+
+  public DisplayWindowImpl(String title, CLImage2D image, CustomRenderer renderer) {
+    frame.setTitle(title);
+    this.image = image;
+    this.renderer = renderer;
+    initComponents();
+    initRun();
+  }
+
+  private void initComponents() {
+    frame.setSize((int) image.getWidth(), (int) image.getHeight());
+    frame.getContentPane().add(label);
+    frame.setResizable(false);
+    frame.addWindowListener(this);
+  }
+
+  private void initRun() {
+    workingRun = new ComputationRun() {
+
+      BufferedImage hostImage;
+
+      @Override
+      public void launch(CLQueue queue) {
+        hostImage = image.read(queue);
+      }
+
+      @Override
+      public void land() {
+        if (renderer != null) {
+          renderer.render(hostImage.createGraphics());
+        }
+        label.setIcon(new ImageIcon(hostImage));
+        ocl.castSignal(SIG_done);
+      }
+    };
+  }
+
+  @Override
+  public void setTitle(String title) {
+    frame.setTitle(title);
+  }
+
+  @Override
+  public String getTitle() {
+    return frame.getTitle();
+  }
+
+  @Override
+  public OCLSignal getSignal() {
+    return SIG_done;
+  }
+
+  @Override
+  public void show() {
+    activate();
+    frame.setVisible(true);
+  }
+
+  @Override
+  public void hide() {
+    deactivate();
+    frame.setVisible(false);
+  }
+
+  // TODO: synchronize
+  private void activate() {
+    if (!active) {
+      ocl.registerLaunch(trigger, workingRun);
+      active = true;
+    }
+  }
+
+  // TODO: synchronize
+  private void deactivate() {
+    if (active) {
+      ocl.unregisterLaunch(trigger, workingRun);
+      active = false;
+    }
+  }
+
+  @Override
+  public boolean isActive() {
+    return active;
+  }
+
+  @Override
+  public void windowClosing(WindowEvent we) {
+    deactivate();
+  }
+
+  @Override
+  public void windowIconified(WindowEvent we) {
+    deactivate();
+  }
+
+  @Override
+  public void windowDeiconified(WindowEvent we) {
+    activate();
+  }
+
+  // <editor-fold defaultstate="collapsed" desc="Unused WindowListener methods">
+  @Override
+  public void windowOpened(WindowEvent we) {
+  }
+
+  @Override
+  public void windowClosed(WindowEvent we) {
+  }
+
+  @Override
+  public void windowActivated(WindowEvent we) {
+  }
+
+  @Override
+  public void windowDeactivated(WindowEvent we) {
+  }
+  // </editor-fold>
+
+  @Override
+  public BufferedImage getImage() {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+}
