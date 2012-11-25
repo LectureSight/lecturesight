4b0a8f558c59694988d42642449d0adeeab5ea36
bwulff <bwulff@e6edf6fb-f266-4316-afb4-e53d95876a76>
2012-11-25 03:50:59 +0000

    LECTURESIGHT-12
    git-svn-id: http://localhost/repos/opencast/LECTURESIGHT/trunk@13268 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ src/modules/lecturesight-display-api/src/main/java/cv/lecturesight/display/DisplayRegistrationListener.java
@@ -0,0 +1,7 @@
+package cv.lecturesight.display;
+
+public interface DisplayRegistrationListener {
+
+  void displayAdded(DisplayRegistration reg);
+  void displayRemoved(DisplayRegistration reg);
+}
