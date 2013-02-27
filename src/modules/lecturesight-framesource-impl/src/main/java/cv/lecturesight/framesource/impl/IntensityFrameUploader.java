d3b72871d89526eb1b38f1b3f4ed790633f53a33
bwulff <bwulff@e6edf6fb-f266-4316-afb4-e53d95876a76>
2013-02-27 13:54:48 +0000

    LECTURESIGHT-43: added IntensityFrameUplaoder
    git-svn-id: http://localhost/repos/opencast/LECTURESIGHT/trunk@13949 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ src/modules/lecturesight-framesource-impl/src/main/java/cv/lecturesight/framesource/impl/IntensityFrameUploader.java
@@ -0,0 +1,124 @@
+package cv.lecturesight.framesource.impl;
+
+import com.nativelibs4java.opencl.CLByteBuffer;
+import com.nativelibs4java.opencl.CLEvent;
+import com.nativelibs4java.opencl.CLImage2D;
+import com.nativelibs4java.opencl.CLImageFormat;
+import com.nativelibs4java.opencl.CLKernel;
+import com.nativelibs4java.opencl.CLMem;
+import com.nativelibs4java.opencl.CLQueue;
+import cv.lecturesight.framesource.FrameGrabber;
+import cv.lecturesight.opencl.OpenCLService;
+import cv.lecturesight.opencl.api.ComputationRun;
+import cv.lecturesight.opencl.api.OCLSignal;
+import java.awt.image.BufferedImage;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.util.UUID;
+
+public class IntensityFrameUploader implements FrameUploader {
+  private final String UID = UUID.randomUUID().toString();
+  private final OpenCLService ocl;
+  private final FrameGrabber grabber;
+  private final CLKernel conversionK, maskK;
+  private final int [] workDim;
+  private final long bufferSize;
+  private ByteBuffer hostBuffer;
+  private CLByteBuffer gpuRawBuffer;
+  private final CLImage2D gpuBuffer, temp;
+  private CLImage2D mask = null;
+  private BufferedImage imageHost;
+  private final OCLSignal sig_start;
+  private final OCLSignal sig_done;
+  private BufferedImage maskImage = null;
+
+  public IntensityFrameUploader(OpenCLService clService, FrameGrabber grabber) {
+    this.ocl = clService;
+    this.grabber = grabber;
+
+    // create signals
+    sig_start = ocl.getSignal(UID + "_START");
+    sig_done = ocl.getSignal(UID + "_DONE");
+
+    // set up gpu buffgers
+    bufferSize = grabber.getWidth() * grabber.getHeight() * 3;
+    gpuRawBuffer = ocl.context().createByteBuffer(CLMem.Usage.InputOutput, bufferSize);
+    gpuBuffer = ocl.context().createImage2D(CLMem.Usage.InputOutput,
+            new CLImageFormat(CLImageFormat.ChannelOrder.BGRA, CLImageFormat.ChannelDataType.UnsignedInt8),
+            grabber.getWidth(), grabber.getHeight());
+    temp = ocl.context().createImage2D(CLMem.Usage.InputOutput,
+            new CLImageFormat(CLImageFormat.ChannelOrder.BGRA, CLImageFormat.ChannelDataType.UnsignedInt8),
+            grabber.getWidth(), grabber.getHeight());
+
+    // set up conversion kernel
+    conversionK = ocl.programs().getKernel("conversions", "Intensity8_RGBAUint8");
+    
+    // set up mask kernel
+    maskK = ocl.programs().getKernel("conversions", "apply_mask");
+
+    workDim = new int[] {grabber.getWidth(), grabber.getHeight()};
+
+    // System.out.println("Computed buffer size: " + bufferSize);
+
+    // set up conversion run
+    ocl.registerLaunch(sig_start, new ComputationRun() {
+
+      @Override
+      public void launch(CLQueue queue) {
+        CLEvent uploadDone = gpuRawBuffer.writeBytes(queue, 0, bufferSize, hostBuffer, false);
+        if (mask != null) {
+          conversionK.setArgs(workDim[0], workDim[1], gpuRawBuffer, temp);
+          conversionK.enqueueNDRange(queue, workDim, uploadDone);
+          maskK.setArgs(temp, mask, gpuBuffer);
+          maskK.enqueueNDRange(queue, workDim);
+        } else {
+          conversionK.setArgs(workDim[0], workDim[1], gpuRawBuffer, gpuBuffer);
+          conversionK.enqueueNDRange(queue, workDim, uploadDone);
+        }
+        imageHost = gpuBuffer.read(queue);
+      }
+
+      @Override
+      public void land() {
+        ocl.castSignal(sig_done);
+      }
+
+    });
+  }
+
+  @Override
+  public OCLSignal getSignal() {
+    return sig_done;
+  }
+
+  @Override
+  public CLImage2D getOutputImage() {
+    return gpuBuffer;
+  }
+  
+  @Override
+  public CLImage2D getRawOutputImage() {
+    if (mask != null) {
+      return temp;
+    } else {
+      return gpuBuffer;
+    }
+  }
+
+  @Override
+  public void upload(Buffer frame) {
+    hostBuffer = (ByteBuffer)frame;     // FIXME not thread-safe!
+    ocl.castSignal(sig_start);
+  }
+
+  @Override
+  public BufferedImage getOutputImageHost() {
+    return imageHost;
+  }
+  
+  @Override
+  public void setMask(BufferedImage mask) {
+    this.maskImage = mask; 
+    this.mask = ocl.context().createImage2D(CLMem.Usage.InputOutput, maskImage, false);
+  }
+}
