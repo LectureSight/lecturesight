16cac8394cab9160a30baa79868e358a21f29157
bwulff <bwulff@e6edf6fb-f266-4316-afb4-e53d95876a76>
2013-03-12 11:06:06 +0000

    LECTURESIGHT-45: removed old felix version, added felix version 4.2
    git-svn-id: http://localhost/repos/opencast/LECTURESIGHT/trunk@14023 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ felix-4.2.0/start_lecturesight.sh
@@ -0,0 +1,7 @@
+DEBUG_PORT="8001"
+DEBUG_SUSPEND="n"
+DEBUG_OPTS="-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=$DEBUG_PORT,server=y,suspend=$DEBUG_SUSPEND"
+FELIX_CACHE="./felix-cache"
+
+rm -rf $FELIX_CACHE/*
+java $DEBUG_OPTS -Dlecturesight.basedir=. -jar bin/felix.jar $FELIX_CACHE  
