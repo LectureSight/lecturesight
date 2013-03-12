6c9e38916d2813e4afad4d0defb7ab7f5199779d
bwulff <bwulff@e6edf6fb-f266-4316-afb4-e53d95876a76>
2013-03-12 10:42:44 +0000

    removed old felix version from trunk
    D    https://opencast.jira.com/svn/LECTURESIGHT/trunk/felix
    
    
    git-svn-id: http://localhost/repos/opencast/LECTURESIGHT/trunk@14021 e6edf6fb-f266-4316-afb4-e53d95876a76

--- felix/start_lecturesight.sh
+++
@@ -1,7 +0,0 @@
-DEBUG_PORT="8000"
-DEBUG_SUSPEND="n"
-DEBUG_OPTS="-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=$DEBUG_PORT,server=y,suspend=$DEBUG_SUSPEND"
-FELIX_CACHE="./felix-cache"
-
-rm -rf $FELIX_CACHE/*
-java $DEBUG_OPTS -Dlecturesight.basedir=. -jar bin/felix.jar $FELIX_CACHE  
