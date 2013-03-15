c896372be39df3f170bc5ed84be51dc13d167ece
bwulff <bwulff@e6edf6fb-f266-4316-afb4-e53d95876a76>
2013-03-15 14:23:50 +0000

    LECTURESIGHT-45: added more module documentations
    git-svn-id: http://localhost/repos/opencast/LECTURESIGHT/trunk@14072 e6edf6fb-f266-4316-afb4-e53d95876a76

--- src/modules/lecturesight-opencl-api/src/main/java/cv/lecturesight/opencl/OCLUtils.java
+++ src/modules/lecturesight-opencl-api/src/main/java/cv/lecturesight/opencl/OCLUtils.java
@@ -21,11 +21,62 @@ import com.nativelibs4java.opencl.CLEvent;
 import com.nativelibs4java.opencl.CLImage2D;
 import com.nativelibs4java.opencl.CLIntBuffer;
 
+/** A collection of utility functions. All functions return a CLEvent so they
+ * can be synchronized with other CL commands.
+ * 
+ * @author wulff
+ */
 public interface OCLUtils {
 
+  /** Set pixel values in a specified rectangle in an RGBA image
+   * 
+   * @param x x position of upper left corner
+   * @param y y position of upper left corner
+   * @param width width of rectangle
+   * @param height height of rectangle
+   * @param image image to be changed
+   * @param r red value of pixels
+   * @oaram g green value of pixels
+   * @param b blue value of pixels
+   * @param a alpha value of pixels
+   * @return CLEvent indicating completion
+   */
   CLEvent setValues(int x, int y, int width, int height, CLImage2D image, int r, int g, int b, int a);
-  CLEvent setValues(int x, int y, int width, int height, CLImage2D image, int val);
-  CLEvent setValues(int start, int end, CLIntBuffer buffer, int val);
 
+  /** Set pixel values in a specified rectangle in an INTENSITY image
+   * 
+   * @param x x position of upper left corner
+   * @param y y position of upper left corner
+   * @param width width of rectangle
+   * @param height height of rectangle
+   * @param image image to be changed
+   * @param val pixel value
+   * @return CLEvent indicating completion
+   */
+  CLEvent setValues(int x, int y, int width, int height, CLImage2D image, int val);
+  
+  /** Set values of a two-dimensional int buffer.
+   * 
+   * @param start start offset
+   * @param end end offset
+   * @param buffer buffer to be changed
+   * @param val value to be set
+   * @return CLEvent indicating completion
+   */
+  CLEvent setValues(int start, int end, CLIntBuffer buffer, int val);
+  
+  /** Copy a defined rectangle from one image into another image. The two images
+   * must be of same type an channel count.
+   * 
+   * @param x x position of upper left corner
+   * @param y y position of upper left corner
+   * @param width width of rectangle
+   * @param height height of rectangle
+   * @param source source image
+   * @param dest_x x position in destination image
+   * @param dest_y y position in destination image
+   * @param dest destination image
+   * @return CLEvent indicating completion
+   */
   CLEvent copyImage(int x, int y, int width, int height, CLImage2D source, int dest_x, int dest_y, CLImage2D dest);
 }
--- src/modules/lecturesight-opencl-api/src/main/java/cv/lecturesight/opencl/OpenCLService.java
+++ src/modules/lecturesight-opencl-api/src/main/java/cv/lecturesight/opencl/OpenCLService.java
@@ -24,11 +24,13 @@ import cv.lecturesight.opencl.api.OCLSignal;
 import cv.lecturesight.opencl.api.OCLSignalBarrier;
 import cv.lecturesight.opencl.api.Triggerable;
 
-/** OpenCL Service API
+/** API for the OpenCL Service
  * 
+ * @author wulff
  */
 public interface OpenCLService {
 
+  // Image formats
   enum Format {
     BGRA_UINT8(new CLImageFormat(CLImageFormat.ChannelOrder.BGRA, CLImageFormat.ChannelDataType.UnsignedInt8)), 
     INTENSITY_UINT8(new CLImageFormat(CLImageFormat.ChannelOrder.INTENSITY, CLImageFormat.ChannelDataType.UnsignedInt8));
@@ -46,22 +48,71 @@ public interface OpenCLService {
     //</editor-fold>
   };
 
+  /** Returns the underlying JavaCL context
+   * 
+   * @return JavaCl context
+   */
   CLContext context();
 
+  /** Returns the <code>OCLProgramStore</code> of this service instance. Every
+   * bundle that uses the OpenCL Service is scanned for OpenCL sources. The
+   * compiled OpenCL programs can be accessed through the program store.
+   * 
+   * @return OpenCL program store
+   */
   OCLProgramStore programs();
 
+  /** Schedule a <ComputationRun> for immediate launch. The <code>run</code> 
+   * will be launched of the next possible occasion.
+   * 
+   * TODO return CLEvent for synchronization
+   * 
+   * @param run ComputationRun to be launched
+   */
   void immediateLaunch(ComputationRun run);
 
+  /** Returns the <code>OCLSignal</code> with the specified name. If a signal
+   * with the specified name is not existing, it will be created and registered
+   * under that name.
+   * 
+   * @param name of the signal
+   * @return OCLSignal registered under <code>name</name>
+   */
   OCLSignal getSignal(String name);
 
+  /** Returns the object that holds the utility functions.
+   * 
+   * @return object with utility functions
+   */
   OCLUtils utils();
 
+  /** Emiits the provided signal the to OpenCL runtime management system.
+   * 
+   * @param signal signal to be emitted.
+   */
   void castSignal(OCLSignal signal);
 
+  /** Register <code>run</code> to be launched when <code>trigger</code> is 
+   * emitted.
+   * 
+   * @param trigger signal that should trigger the launch
+   * @param run ComputationRun to be launched on trigger
+   */
   void registerLaunch(OCLSignal trigger, ComputationRun run);
 
+  /** Register <code>run</code> to be launched when any of the <code>triggers</code> 
+   * is emitted.
+   * 
+   * @param triggers signals that should trigger the launch
+   * @param run ComputationRun to be launched on trigger
+   */
   void registerLaunch(OCLSignal[] triggers, ComputationRun run);
 
+  /** Unregister <code>run</code> from launch on <code>trigger</code>. 
+   * 
+   * @param trigger signal from which the ComputationRun should be detached
+   * @param run ComputationRun to be detached from signal
+   */
   void unregisterLaunch(OCLSignal trigger, ComputationRun run);
 
   void registerTriggerable(OCLSignal trigger, Triggerable handler);
@@ -71,5 +122,4 @@ public interface OpenCLService {
   void unregisterTriggerable(OCLSignal trigger, Triggerable handler);
 
   OCLSignalBarrier createSignalBarrier(OCLSignal[] triggers);
-
 }
