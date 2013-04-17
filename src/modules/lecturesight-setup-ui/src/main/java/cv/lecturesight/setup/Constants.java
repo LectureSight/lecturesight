1abde9ddb4948b28a90e3e997545f6c04af7762c
bwulff <bwulff@e6edf6fb-f266-4316-afb4-e53d95876a76>
2013-04-17 15:50:02 +0000

    LECTURESIGHT-50: updated setup ui; updated frame source infrastructure
    git-svn-id: http://localhost/repos/opencast/LECTURESIGHT/trunk@14329 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ src/modules/lecturesight-setup-ui/src/main/java/cv/lecturesight/setup/Constants.java
@@ -0,0 +1,10 @@
+package cv.lecturesight.setup;
+
+public class Constants {
+
+  static final String PROPKEY_CALIB_FRAMESOURCE = "cv.lecturesight.camcalibration.video.input";
+  static final String PROPKEY_CO_LEFT = "cv.lecturesight.cameraoperator.bound.left";
+  static final String PROPKEY_CO_RIGHT = "cv.lecturesight.cameraoperator.bound.right";
+  static final String PROPKEY_CO_TOP = "cv.lecturesight.cameraoperator.bound.top";
+  static final String PROPKEY_CO_BOTTOM = "cv.lecturesight.cameraoperator.bound.bottom";
+}
