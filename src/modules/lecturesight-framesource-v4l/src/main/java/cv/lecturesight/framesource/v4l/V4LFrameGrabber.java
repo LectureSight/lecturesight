c594e08d8df32090865292970ba5e2f95252f04f
ts23 <ts23gm@gmail.com>
2014-03-05 11:20:49 +0000

    Revert "update to v4l4j-0.9.1"
    This reverts commit 05958b0bc1f01acb0b30c6fae26ff389d0a04067.

--- src/modules/lecturesight-framesource-v4l/src/main/java/cv/lecturesight/framesource/v4l/V4LFrameGrabber.java
+++ src/modules/lecturesight-framesource-v4l/src/main/java/cv/lecturesight/framesource/v4l/V4LFrameGrabber.java
@@ -17,10 +17,8 @@
  */
 package cv.lecturesight.framesource.v4l;
 
-import au.edu.jcu.v4l4j.CaptureCallback;
 import au.edu.jcu.v4l4j.FrameGrabber;
 import au.edu.jcu.v4l4j.VideoDevice;
-import au.edu.jcu.v4l4j.VideoFrame;
 import au.edu.jcu.v4l4j.exceptions.V4L4JException;
 import cv.lecturesight.framesource.FrameGrabber.PixelFormat;
 import cv.lecturesight.framesource.FrameSourceException;
@@ -28,19 +26,12 @@ import cv.lecturesight.util.Log;
 import java.nio.Buffer;
 import java.nio.ByteBuffer;
 
-public class V4LFrameGrabber implements cv.lecturesight.framesource.FrameGrabber, CaptureCallback {
-
-  @Override
-  public void exceptionReceived(V4L4JException vlje) {
-    throw new UnsupportedOperationException(
-      new FrameSourceException("Could not capture frame from " + device.getDevicefile() + ": " + vlje.getMessage())); 
-  }
+public class V4LFrameGrabber implements cv.lecturesight.framesource.FrameGrabber {
 
   private Log log;
   VideoDevice device;
   int width, height, standard, channel, quality;
   private FrameGrabber grabber;
-  private ByteBuffer frameBuffer;
 
   V4LFrameGrabber(VideoDevice device, int frameWidth, int frameHeight, int videoStandard,
     int videoChannel, int videoQuality) throws FrameSourceException {
@@ -54,8 +45,6 @@ public class V4LFrameGrabber implements cv.lecturesight.framesource.FrameGrabber
     try {
       log = new Log(device.getDeviceInfo().getName());
       grabber = device.getRGBFrameGrabber(width, height, channel, standard);
-      frameBuffer = null;
-      grabber.setCaptureCallback(this);
       grabber.startCapture();
     } catch (V4L4JException e) {
       throw new FrameSourceException("Failed to initialize FrameGrabber on device " + device.getDevicefile());
@@ -78,18 +67,13 @@ public class V4LFrameGrabber implements cv.lecturesight.framesource.FrameGrabber
   }
 
   @Override
-  public void nextFrame(VideoFrame frame) {
-    frameBuffer = ByteBuffer.wrap(frame.getBytes());
-    frame.recycle();
-  }
- 
-  @Override
   public Buffer captureFrame() throws FrameSourceException {
-      if (frameBuffer != null) {
-          return frameBuffer;
-      } else {
-          throw new FrameSourceException("Could not capture frame from " + device.getDevicefile());
-      }
+    try {
+      ByteBuffer bb = grabber.getFrame();
+      return bb;
+    } catch (V4L4JException ex) {
+      throw new FrameSourceException("Could not capture frame from " + device.getDevicefile() + ": " + ex.getMessage());
+    }
   }
 
   void shutdown() {
