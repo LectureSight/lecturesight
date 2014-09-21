28125fa49bc66ef18bdc0c25efc2f628431eb697
Wulff <bwulff@uos.de>
2014-09-21 19:42:14 +0200

    LS-42: added RGB and Depth FrameSources for Microsoft Kinect.

--- src/modules/lecturesight-framesource-kinect/src/main/java/cv/lecturesight/framesource/kinect/KinectDepthFrameGrabber.java
+++ src/modules/lecturesight-framesource-kinect/src/main/java/cv/lecturesight/framesource/kinect/KinectDepthFrameGrabber.java
@@ -3,8 +3,28 @@ package cv.lecturesight.framesource.kinect;
 import cv.lecturesight.framesource.FrameGrabber;
 import cv.lecturesight.framesource.FrameSourceException;
 import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import org.openkinect.freenect.DepthHandler;
+import org.openkinect.freenect.FrameMode;
 
-public class KinectDepthFrameGrabber implements FrameGrabber {
+public class KinectDepthFrameGrabber implements FrameGrabber, DepthHandler {
+
+  int devIndex;
+  private ByteBuffer currentFrame = null;
+
+  public KinectDepthFrameGrabber(int devIndex) {
+    this.devIndex = devIndex;
+  }
+
+  @Override
+  public Buffer captureFrame() throws FrameSourceException {
+    return currentFrame;
+  }
+
+  @Override
+  public void onFrameReceived(FrameMode fm, ByteBuffer bb, int i) {
+    currentFrame = bb;
+  }
 
   @Override
   public int getWidth() {
@@ -20,10 +40,4 @@ public class KinectDepthFrameGrabber implements FrameGrabber {
   public PixelFormat getPixelFormat() {
     return PixelFormat.INTENSITY_8BIT;
   }
-
-  @Override
-  public Buffer captureFrame() throws FrameSourceException {
-    throw new UnsupportedOperationException("Not supported yet.");
-  }
-  
 }
