d3ee74bb085bbd5d230a0b160d7a13d1cb5f51d3
Benjamin Wulff <bwulff@uos.de>
2015-01-12 15:30:25 +0100

    added Scripting API bundle

---
+++ src/modules/lecturesight-scripting-api/src/main/java/cv/lecturesight/scripting/api/Bridge.java
@@ -0,0 +1,8 @@
+package cv.lecturesight.scripting.api;
+
+public interface Bridge {
+  
+  public String getScriptName();
+  public String[] getImports();
+  
+}
---
+++ src/modules/lecturesight-scripting-api/src/main/java/cv/lecturesight/scripting/api/ScriptRunner.java
@@ -0,0 +1,6 @@
+package cv.lecturesight.scripting.api;
+
+public abstract class ScriptRunner {
+  
+  
+}
