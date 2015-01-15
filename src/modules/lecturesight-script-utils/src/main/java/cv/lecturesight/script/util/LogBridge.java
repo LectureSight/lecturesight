1c5398c0cb566379fb6872d0db0abd02f431f2cb
Benjamin Wulff <bwulff@uos.de>
2015-01-15 13:18:47 +0100

    added Log API implementation

---
+++ src/modules/lecturesight-script-utils/src/main/java/cv/lecturesight/script/util/LogBridge.java
@@ -0,0 +1,40 @@
+package cv.lecturesight.script.util;
+
+import cv.lecturesight.scripting.api.ScriptBridge;
+import cv.lecturesight.scripting.api.ScriptParent;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Service;
+
+@Component(name="lecturesight.script.util.time", immediate=true)
+@Service()
+@Properties({
+ @Property(name="bridge.name", value="Log"),
+ @Property(name="bridge.imports", value="")  
+})
+public class LogBridge implements ScriptBridge {
+
+  ScriptParent parent;
+  
+  public void debug(String msg) {
+    parent.getLogger().debug(msg);
+  }
+  
+  public void info(String msg) {
+    parent.getLogger().info(msg);
+  }
+  
+  public void warn(String msg) {
+    parent.getLogger().warn(msg);
+  }
+  
+  public void error(String msg) {
+    parent.getLogger().error(msg);
+  }
+  
+  @Override
+  public void setScriptParent(ScriptParent p) {
+    this.parent = p;
+  }
+}
