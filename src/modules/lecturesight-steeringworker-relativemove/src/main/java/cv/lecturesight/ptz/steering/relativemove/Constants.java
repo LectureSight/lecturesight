cf2536e2fa5405d560baa45309d72a44a8d84956
Benjamin Wulff <benjamin.wulff@iais.fraunhofer.de>
2015-11-25 11:59:09 +0100

    LS-136: corrected property keys in Relative Move Steering Worker

--- src/modules/lecturesight-steeringworker-relativemove/src/main/java/cv/lecturesight/ptz/steering/relativemove/Constants.java
+++ src/modules/lecturesight-steeringworker-relativemove/src/main/java/cv/lecturesight/ptz/steering/relativemove/Constants.java
@@ -2,15 +2,15 @@ package cv.lecturesight.ptz.steering.relativemove;
 
 public class Constants {
 
-  static final String PROPKEY_AUTOSTART ="cv.lecturesight.ptz.steering.worker.autostart";
-  static final String PROPKEY_XFLIP = "cv.lecturesight.ptz.steering.worker.xflip";
-  static final String PROPKEY_YFLIP = "cv.lecturesight.ptz.steering.worker.yflip";
-  static final String PROPKEY_ALPHAX = "cv.lecturesight.ptz.steering.worker.move.alpha.x";
-  static final String PROPKEY_ALPHAY = "cv.lecturesight.ptz.steering.worker.move.alpha.y";
-  static final String PROPKEY_DAMP_PAN = "cv.lecturesight.ptz.steering.worker.move.damp.pan";
-  static final String PROPKEY_DAMP_TILT = "cv.lecturesight.ptz.steering.worker.move.damp.tilt";
-  static final String PROPKEY_LIMIT_LEFT = "cv.lecturesight.ptz.steering.worker.scene.limit.left";
-  static final String PROPKEY_LIMIT_RIGHT = "cv.lecturesight.ptz.steering.worker.scene.limit.right";
-  static final String PROPKEY_LIMIT_TOP = "cv.lecturesight.ptz.steering.worker.scene.limit.top";
-  static final String PROPKEY_LIMIT_BOTTOM = "cv.lecturesight.ptz.steering.worker.scene.limit.bottom";
+  static final String PROPKEY_AUTOSTART ="cv.lecturesight.ptz.steering.worker.relativemove.autostart";
+  static final String PROPKEY_XFLIP = "cv.lecturesight.ptz.steering.worker.relativemove.xflip";
+  static final String PROPKEY_YFLIP = "cv.lecturesight.ptz.steering.worker.relativemove.yflip";
+  static final String PROPKEY_ALPHAX = "cv.lecturesight.ptz.steering.worker.relativemove.move.alpha.x";
+  static final String PROPKEY_ALPHAY = "cv.lecturesight.ptz.steering.worker.relativemove.move.alpha.y";
+  static final String PROPKEY_DAMP_PAN = "cv.lecturesight.ptz.steering.worker.relativemove.move.damp.pan";
+  static final String PROPKEY_DAMP_TILT = "cv.lecturesight.ptz.steering.worker.relativemove.move.damp.tilt";
+  static final String PROPKEY_LIMIT_LEFT = "cv.lecturesight.ptz.steering.worker.relativemove.scene.limit.left";
+  static final String PROPKEY_LIMIT_RIGHT = "cv.lecturesight.ptz.steering.worker.relativemove.scene.limit.right";
+  static final String PROPKEY_LIMIT_TOP = "cv.lecturesight.ptz.steering.worker.relativemove.scene.limit.top";
+  static final String PROPKEY_LIMIT_BOTTOM = "cv.lecturesight.ptz.steering.worker.relativemove.scene.limit.bottom";
 }
