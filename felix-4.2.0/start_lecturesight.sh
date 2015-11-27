1ab98810d67754536d38e701896d6b2fcf2ea81c
Benjamin Wulff <benjamin.wulff@iais.fraunhofer.de>
2015-11-27 14:18:27 +0100

    LS-141 added tinylog configuration file path to start script

--- felix-4.2.0/start_lecturesight.sh
+++ felix-4.2.0/start_lecturesight.sh
@@ -1,6 +1,7 @@
 DEBUG_PORT="8001"
 DEBUG_SUSPEND="n"
 DEBUG_OPTS="-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=$DEBUG_PORT,server=y,suspend=$DEBUG_SUSPEND"
+LOG_OPTS="-Dtinylog.configuration=conf/log.properties"
 FELIX_CACHE="./felix-cache"
 
 rm -rf $FELIX_CACHE/*
