b19ca95e3efaff5fd8a2aa076923df8b68fc200f
Stephen Marquard <stephen.marquard@uct.ac.za>
2017-11-21 13:45:24 +0200

    LS-164 Dispose of the sample properly to avoid gstreamer complaints
    Following the example here:
    
    https://github.com/gstreamer-java/gst1-java-examples/blob/master/src/main/java/org/freedesktop/gstreamer/examples/SimpleVideoComponent.java
    
    the buffer should be unmapped, and the sample disposed after use. This avoids a gstreamer assertion error that sometimes arose when
    unmapping the buffer.

--- src/modules/lecturesight-framesource-rtph264/src/main/java/cv/lecturesight/framesource/rtph264/RTPH264ClientFrameGrabber.java
+++ src/modules/lecturesight-framesource-rtph264/src/main/java/cv/lecturesight/framesource/rtph264/RTPH264ClientFrameGrabber.java
@@ -25,6 +25,7 @@ import org.freedesktop.gstreamer.Caps;
 import org.freedesktop.gstreamer.Element;
 import org.freedesktop.gstreamer.ElementFactory;
 import org.freedesktop.gstreamer.Pipeline;
+import org.freedesktop.gstreamer.Sample;
 import org.freedesktop.gstreamer.State;
 import org.freedesktop.gstreamer.Structure;
 import org.freedesktop.gstreamer.elements.AppSink;
@@ -39,8 +40,8 @@ public class RTPH264ClientFrameGrabber implements FrameGrabber {
   private int width, height;
   private ByteBuffer lastFrame;
   private Buffer lastBuf;
+  private Sample lastSam;
   private AppSink appsink;
-
 
   public RTPH264ClientFrameGrabber(String host, int port) throws FrameSourceException {
     this.host = host;
@@ -135,14 +136,17 @@ public class RTPH264ClientFrameGrabber implements FrameGrabber {
   @Override
   public ByteBuffer captureFrame() throws FrameSourceException {
     if (!appsink.isEOS()) {
-      Buffer buf = appsink.pullSample().getBuffer();
+      Sample sam = appsink.pullSample();
+      Buffer buf = sam.getBuffer();
       if (buf != null) {
         lastFrame = buf.map(false);
         if (lastBuf != null) {
            // Free memory allocated for the previous buffer so we don't leak memory
            lastBuf.unmap();
+           lastSam.dispose();
         }
         lastBuf = buf;
+        lastSam = sam;
       } else {
         Logger.warn("Buffer is NULL!!");
       }
