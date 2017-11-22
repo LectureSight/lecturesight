00ac5c465261033befb3009728f678d0e67b24ed
Stephen Marquard <stephen.marquard@uct.ac.za>
2017-11-22 21:16:11 +0200

    LS-207 Unused import

--- src/modules/lecturesight-videoanalysis-templ/src/main/java/cv/lecturesight/videoanalysis/templ/VideoAnalysisTemplateMatching.java
+++ src/modules/lecturesight-videoanalysis-templ/src/main/java/cv/lecturesight/videoanalysis/templ/VideoAnalysisTemplateMatching.java
@@ -30,7 +30,6 @@ import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
 import org.osgi.service.component.ComponentContext;
-import org.pmw.tinylog.Level;
 import org.pmw.tinylog.Logger;
 
 import java.nio.IntBuffer;
