f2618b4768c9977014e29c0fb7349694654d7e12
James S Perrin <james.s.perrin@manchester.ac.uk>
2017-11-23 15:58:32 +0000

    LS-194, add a simple ptz camera operator
    * no zoom adjustment currently
    * tilt can be offset
    * tilt can be locked to provide pan-only mode

---
+++ src/modules/lecturesight-cameraoperator-ptz/src/main/java/cv/lecturesight/cameraoperator/ptz/Constants.java
@@ -0,0 +1,15 @@
+package cv.lecturesight.cameraoperator.ptz;
+
+public class Constants {
+  static final String PROPKEY_TARGET_TIMEOUT = "target.timeout";
+  static final String PROPKEY_TRACKING_TIMEOUT = "tracking.timeout";
+  static final String PROPKEY_PAN = "pan";
+  static final String PROPKEY_TILT = "tilt";
+  static final String PROPKEY_ZOOM = "zoom";
+  static final String PROPKEY_FRAME_WIDTH = "frame.width";
+  static final String PROPKEY_FRAME_HEIGHT = "frame.height";
+  static final String PROPKEY_IDLE_PRESET = "idle.preset";
+  static final String PROPKEY_TARGET_LIMIT = "target.limit";
+  static final String PROPKEY_TILT_LOCK = "tilt.lock";
+  static final String PROPKEY_TILT_OFFSET = "tilt.offset";
+}
---
+++ src/modules/lecturesight-cameraoperator-ptz/src/main/java/cv/lecturesight/cameraoperator/ptz/SimplePanTiltZoomCameraOperator.java
@@ -0,0 +1,280 @@
+/* Copyright (C) 2012 Benjamin Wulff
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+package cv.lecturesight.cameraoperator.ptz;
+
+import org.pmw.tinylog.Logger;
+import cv.lecturesight.framesource.FrameSource;
+import cv.lecturesight.framesource.FrameSourceProvider;
+import cv.lecturesight.objecttracker.ObjectTracker;
+import cv.lecturesight.objecttracker.TrackerObject;
+import cv.lecturesight.operator.CameraOperator;
+import cv.lecturesight.ptz.steering.api.CameraSteeringWorker;
+import cv.lecturesight.util.conf.Configuration;
+import cv.lecturesight.util.conf.ConfigurationListener;
+import cv.lecturesight.util.geometry.CoordinatesNormalization;
+import cv.lecturesight.util.geometry.NormalizedPosition;
+import cv.lecturesight.util.geometry.Position;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
+import org.osgi.service.component.ComponentContext;
+
+@Component(name = "lecturesight.cameraoperator.ptz", immediate = true)
+@Service
+public class SimplePanTiltZoomCameraOperator implements CameraOperator, ConfigurationListener {
+
+  @Reference
+  Configuration config;
+
+  @Reference
+  ObjectTracker tracker;
+
+  @Reference
+  CameraSteeringWorker steerer;
+
+  @Reference
+  FrameSourceProvider fsp;
+  FrameSource fsrc;
+
+  int interval = 200;
+  int target_timeout;
+  int tracking_timeout;
+  int idle_preset = -1;
+  int target_limit = 10;
+
+  CoordinatesNormalization normalizer;
+  ScheduledExecutorService executor;
+  CameraOperatorWorker worker;
+
+  float start_pan = 0;
+  float start_tilt = 0;
+  float start_zoom = 0;
+  float frame_width = 0;
+  float frame_height = 0;
+  boolean tilt_lock = true;
+  float tilt_offset = 0;
+
+  protected void activate(ComponentContext cc) throws Exception {
+    setConfiguration();
+    Logger.info("Activated");
+
+    fsrc = fsp.getFrameSource();
+    normalizer = new CoordinatesNormalization(fsrc.getWidth(), fsrc.getHeight());
+    start();
+  }
+
+  protected void deactivate(ComponentContext cc) {
+    stop();
+    Logger.info("Deactivated");
+  }
+
+  /*
+   ** Set configuration values
+   */
+  private void setConfiguration() {
+    target_timeout = config.getInt(Constants.PROPKEY_TARGET_TIMEOUT);
+    tracking_timeout = config.getInt(Constants.PROPKEY_TRACKING_TIMEOUT);
+    idle_preset = config.getInt(Constants.PROPKEY_IDLE_PRESET);
+    start_pan = config.getFloat(Constants.PROPKEY_PAN);
+    start_tilt = config.getFloat(Constants.PROPKEY_TILT);
+    start_zoom = config.getFloat(Constants.PROPKEY_ZOOM);
+    tilt_lock = config.getBoolean(Constants.PROPKEY_TILT_LOCK);
+    if(tilt_lock) {
+      tilt_offset = 0;
+    } else {
+      tilt_offset = config.getFloat(Constants.PROPKEY_TILT_OFFSET);
+    }
+    frame_width = config.getFloat(Constants.PROPKEY_FRAME_WIDTH);
+    frame_height = config.getFloat(Constants.PROPKEY_FRAME_HEIGHT);
+    target_limit = config.getInt(Constants.PROPKEY_TARGET_LIMIT);
+
+    Logger.debug("Target timeout: " + target_timeout + " ms, tracking timeout: " + tracking_timeout + " ms"
+            + ", idle.preset: " + idle_preset
+            + ", initial pan: " + start_pan + ", tilt: " + start_tilt + ", zoom: " + start_zoom
+            + ", frame.width: " + frame_width + ", frame.height: " + frame_height + ", target.limit: " + target_limit
+            + ", tilt.lock: " + tilt_lock+ ", tilt.offset: " + tilt_offset);
+  }
+
+  @Override
+  public void configurationChanged() {
+    Logger.debug("Refreshing configuration");
+    setConfiguration();
+  }
+
+  @Override
+  public void start() {
+    if (executor == null) {
+      executor = Executors.newScheduledThreadPool(1);
+      worker = new CameraOperatorWorker();
+
+      setInitialTrackingPosition();
+      steerer.setSteering(true);
+
+      executor.scheduleAtFixedRate(worker, 0, interval, TimeUnit.MILLISECONDS);
+      Logger.info("Started");
+    }
+  }
+
+  @Override
+  public void stop() {
+    if (executor != null) {
+      executor.shutdownNow();
+      executor = null;
+      Logger.debug("Stopped worker thread");
+    }
+
+    steerer.setSteering(false);
+    setIdlePosition();
+    Logger.info("Stopped");
+  }
+
+  @Override
+  public void reset() {
+    Logger.debug("Reset");
+    setInitialTrackingPosition();
+  }
+
+  @Override
+  public boolean isRunning() {
+    return steerer.isSteering();
+  }
+
+  /*
+   * Move the camera to the initial pan/tilt/zoom position for start of tracking
+   */
+  private void setInitialTrackingPosition() {
+    Logger.debug("Set initial tracking position");
+    NormalizedPosition neutral = new NormalizedPosition(start_pan, start_tilt);
+    steerer.setZoom(start_zoom);
+    steerer.setInitialPosition(neutral);
+  }
+
+  /*
+   * Return the camera to the start pan/tilt position
+   */
+  private void returnInitialTrackingPosition() {
+    Logger.debug("Return to initial tracking position");
+    NormalizedPosition neutral = new NormalizedPosition(start_pan, start_tilt);
+    steerer.setTargetPosition(neutral);
+  }
+
+  /*
+   * Move the camera to the idle position (not tracking)
+   */
+  private void setIdlePosition() {
+    if (idle_preset >= 0) {
+      steerer.movePreset(idle_preset);
+    } else {
+      steerer.moveHome();
+    }
+  }
+
+  private class CameraOperatorWorker implements Runnable {
+
+    TrackerObject target = null;
+    long last_tracked_time;
+
+    @Override
+    public void run() {
+
+      long now = System.currentTimeMillis();
+
+      if (target == null) {
+
+        // Get the list of available targets
+        List<TrackerObject> objs = tracker.getCurrentlyTracked();
+
+        // Find the best target if more than one is available
+        if ((objs.size() > 0) && (objs.size() <= target_limit)) {
+          // only track one target at a time
+          target = findBestTrackedObject(objs);
+        }
+
+        // Return to the initial position if no targets have been visible for a while
+        if ((objs.isEmpty() || target == null) && (tracking_timeout > 0) && (last_tracked_time > 0) && (now - last_tracked_time > tracking_timeout)) {
+          returnInitialTrackingPosition();
+          last_tracked_time = 0;
+        }
+
+      } else if (now - target.lastSeen() < target_timeout) {
+
+        boolean move = true;
+        last_tracked_time = now;
+
+        // Target position
+        Position obj_pos = (Position) target.getProperty(ObjectTracker.OBJ_PROPKEY_CENTROID);
+        NormalizedPosition target_pos = normalizer.toNormalized(obj_pos);
+
+        if (tilt_lock) {
+          target_pos.setY(start_tilt);
+        } else {
+          // ObjectTracker has y +ve == DOWN
+          target_pos.setY(-target_pos.getY() + tilt_offset);
+        }
+
+        // Actual position
+        NormalizedPosition actual_pos = steerer.getActualPosition();
+
+        Logger.debug("Tracking object " + target + " currently at position " + target_pos);
+
+        // Reduce pan activity - only start moving camera if the target is approaching the frame boundaries
+        if ((frame_width > 0 || frame_height > 0) && !steerer.isMoving()) {
+          // In Steerer Model Coords (y +ve == UP )
+          double trigger_left = actual_pos.getX() - (frame_width / 2);
+          double trigger_right = actual_pos.getX() + (frame_width / 2);
+          double trigger_up = actual_pos.getY() + (frame_height / 2);
+          double trigger_down = actual_pos.getY() - (frame_height / 2);
+          if ((target_pos.getX() < trigger_right) && (target_pos.getX() > trigger_left)
+                  && (tilt_lock || ((target_pos.getY() < trigger_up) &&  (target_pos.getY() > trigger_down)))) {
+            move = false;
+            Logger.debug("Not moving: camera=" + actual_pos + " target=" + target_pos
+                    + " position is inside frame trigger limits " +
+                    String.format("%.4f to %.4f, %.4f to %.4f", trigger_left, trigger_right, trigger_down, trigger_up));
+          }
+        }
+
+        if (move) {
+          Logger.debug("Moving steerer for object " + target + " to position " + target_pos);
+          steerer.setTargetPosition(target_pos);
+        }
+
+      } else {
+        target = null;
+      }
+    }
+
+    private TrackerObject findBestTrackedObject(List<TrackerObject> objects) {
+
+      // Track the oldest object
+      TrackerObject out = null;
+      long oldest = System.currentTimeMillis();
+
+      for (TrackerObject obj : objects) {
+        if (obj.firstSeen() < oldest) {
+          oldest = obj.firstSeen();
+          out = obj;
+        }
+      }
+      return out;
+    }
+  }
+}
