e179da8f1dea0cfe1610bd08650876cae6e82de7
Stephen Marquard <stephen.marquard@uct.ac.za>
2017-11-26 15:43:12 +0200

    LS-217 Code cleanup: lecturesight-ptzcontrol-vapix

--- src/modules/lecturesight-ptzcontrol-vapix/src/main/java/cv/lecturesight/vapix/service/FloatRange.java
+++ src/modules/lecturesight-ptzcontrol-vapix/src/main/java/cv/lecturesight/vapix/service/FloatRange.java
@@ -1,63 +1,61 @@
 package cv.lecturesight.vapix.service;
 
-import java.util.Properties;
-
 /**
  * Range of Float values
  */
 public class FloatRange {
 
-	protected float min;
-	protected float max;
+  protected float min;
+  protected float max;
 
-	public FloatRange() {
-		min = 0f;
-		max = 0f;
-	}
-	
-	public FloatRange(float val) {
-		min = val;
-		max = val;
-	}
-	
-	public FloatRange(float _min, float _max) {
-		min = _min;
-		max = _max;
-	}
-	
-	/**
-	 * Return min value
-	 * 
-	 */
-	public float getMin() {
-		return min;
-	}
+  public FloatRange() {
+    min = 0f;
+    max = 0f;
+  }
 
-	/**
-	 * Set the min value
-	 * 
-	 */
-	public void setMin(float value) {
-		this.min = value;
-	}
+  public FloatRange(float val) {
+    min = val;
+    max = val;
+  }
 
-	/**
-	 * Return max
-	 * 
-	 */
-	public float getMax() {
-		return max;
-	}
+  public FloatRange(float _min, float _max) {
+    min = _min;
+    max = _max;
+  }
 
-	/**
-	 * Set the max value
-	 * 
-	 */
-	public void setMax(float value) {
-		this.max = value;
-	}
+  /**
+   * Return min value
+   *
+   */
+  public float getMin() {
+    return min;
+  }
 
-	public String toStrin() {
-		return "(("+ min +" -> "+ max+"))";		
-	}
+  /**
+   * Set the min value
+   *
+   */
+  public void setMin(float value) {
+    this.min = value;
+  }
+
+  /**
+   * Return max
+   *
+   */
+  public float getMax() {
+    return max;
+  }
+
+  /**
+   * Set the max value
+   *
+   */
+  public void setMax(float value) {
+    this.max = value;
+  }
+
+  public String toStrin() {
+    return "(("+ min +" -> "+ max+"))";
+  }
 }
