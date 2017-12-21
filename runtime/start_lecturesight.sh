0436c51d4ea0be1ba4c44be5b39aff49e5bceac2
James S Perrin <james.s.perrin@manchester.ac.uk>
2017-12-21 14:51:58 +0000

    LS-277, use start_lecturesight.sh debug

--- runtime/start_lecturesight.sh
+++ runtime/start_lecturesight.sh
@@ -7,14 +7,23 @@ BASE_DIR="."
 # Available options: CPU, GPU, ACCELERATOR, DEFAULT, ALL
 OPENCL_DEVICE="GPU"
 
+# Debug options. 
+DEBUG_PORT="8001"
+DEBUG_SUSPEND="y"
+
 # assemble options
 FELIX_CACHE="$BASE_DIR/felix-cache"
 LOG_OPTS="-Dtinylog.configuration=$BASE_DIR/conf/log.properties"
 OPENCL_OPTS="-Docl.device.type=$OPENCL_DEVICE -Docl.profiling=no"
 CONFIG_OPTS="-Dfelix.config.properties=file:$BASE_DIR/conf/config.properties -Dfelix.fileinstall.dir=$BASE_DIR/conf/fileinstall/"
+if [ "$#" = "1" ] && [ "$1" = "debug" ] ; then
+  DEBUG_OPTS="-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=$DEBUG_PORT,server=y,suspend=$DEBUG_SUSPEND"
+else
+  DEBUG_OPTS=""
+fi
 
 # erase felix cache
 rm -rf $FELIX_CACHE/*
 
 # start LectureSight
-java -Dlecturesight.basedir=$BASE_DIR $CONFIG_OPTS $LOG_OPTS $OPENCL_OPTS -jar $BASE_DIR/bin/felix.jar -b $BASE_DIR/bundles/system $FELIX_CACHE
+java -Dlecturesight.basedir=$BASE_DIR $CONFIG_OPTS $LOG_OPTS $OPENCL_OPTS $DEBUG_OPTS -jar $BASE_DIR/bin/felix.jar -b $BASE_DIR/bundles/system $FELIX_CACHE
